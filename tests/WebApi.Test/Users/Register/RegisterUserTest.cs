c34f064cb83386f8b07bcaa91df890a65c072268
Leonardo Machado <leonardomachado200112@gmail.com>
2025-06-04 09:23:43 -0300

    custom webfactory

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,11 +1,27 @@
-using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+using CommonTestUtilities.Requests;
+using Shouldly;
 
 namespace WebApi.Test.Users.Register;
 
-public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 {
+    private readonly HttpClient _httpClient;
+    private readonly string METHOD = "api/user";
+
+    public RegisterUserTest(CustomWebApplicationFactory webAppFactory)
+    {
+        _httpClient = webAppFactory.CreateClient();
+    }
+
     [Fact]
     public async Task Success()
     {
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.ShouldBe(HttpStatusCode.Created);
     }
 }
