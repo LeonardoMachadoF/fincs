a6af2f7f31935f7ab60024511dd234b7a9d654c6
Leonardo Machado <leonardomachado200112@gmail.com>
2025-08-31 21:23:42 -0300

    get profile use case test

---
+++ tests/UseCases.Test/Users/Profile/GetUserProfileUseCaseTest.cs
@@ -0,0 +1,33 @@
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using FinCs.Application.UseCases.Users.Profile;
+using FinCs.Domain.Entities;
+using Shouldly;
+
+namespace UseCases.Test.Users.Profile;
+
+public class GetUserProfileUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var result = await useCase.Execute();
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(user.Name);
+        result.Email.ShouldBe(user.Email);
+    }
+
+
+    private GetUserProfileUseCase CreateUseCase(User user)
+    {
+        var mapper = MapperBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new GetUserProfileUseCase(loggedUser, mapper);
+    }
+}
